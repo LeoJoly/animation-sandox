1675b6b89b2213539c863178888d82c3006f849a
LeoJoly <joly.leo@gmail.com>
2023-03-27 11:30:16 +0200

    [feature] - move model on click

--- components/polygone/Polygone.vue
+++ components/polygone/Polygone.vue
@@ -1,43 +1,45 @@
 <script setup lang="ts">
 import { Camera, GltfModel, PointLight, Renderer, Scene, SpotLight } from 'troisjs'
+import { POSITIONS } from '@/scripts/constants'
 
 /** State */
-const cameraPos = reactive({ x: 0, y: 0, z: 3 })
+const positionIndex = ref(0)
 const isModelReady = ref(false)
-const modelPos = reactive({ x: 0, y: -0.5, z: 0 })
-
-/** Template refs */
 const camera = ref<null | typeof Camera>(null)
 const model = ref<null | typeof GltfModel>(null)
 const renderer = ref<null | typeof Renderer>(null)
 
+/** Computed */
+const cameraPos = computed(() => {
+  return POSITIONS[positionIndex.value].camera
+})
+
+const modelPos = computed(() => {
+  return POSITIONS[positionIndex.value].model
+})
+
+/** Methods */
 const onReady = () => {
   isModelReady.value = true
 }
 
 const onError = (error: Error) => {
+  // eslint-disable-next-line no-console
   console.log('error', error)
 }
 
-onMounted(() => {
-  nextTick(() => {
-    if (!renderer.value) return
-    const orbitCtrl = renderer.value.three.cameraCtrl
-    orbitCtrl.addEventListener('change', () => {
-      const { x, y, z } = orbitCtrl.object.position
-      cameraPos.x = x
-      cameraPos.y = y
-      cameraPos.z = z
-    })
-  })
-})
+const onClick = () => {
+  if (!isModelReady.value) return
+  if (positionIndex.value === POSITIONS.length - 1) return
+  positionIndex.value++
+}
 </script>
 
 <template>
   <div class="c-polygone">
     <ClientOnly>
-      <Renderer ref="renderer" resize="window" orbit-ctrl>
-        <Camera ref="camera" :position="{ z: 3 }" />
+      <Renderer ref="renderer" resize="window" orbit-ctrl @click="onClick">
+        <Camera ref="camera" :position="cameraPos" />
         <Scene background="#000000">
           <!-- Red lights -->
           <PointLight color="#EB1006" :position="{ x: 100, y: 0, z: 40 }" :intensity="1" />
@@ -56,51 +58,6 @@ onMounted(() => {
         </Scene>
       </Renderer>
     </ClientOnly>
-    <div class="c-model-control">
-      <form>
-        <p>Position:</p>
-        <div>
-          <label for="modelPosX">X {{ modelPos.x }}</label>
-          <input
-            id="modelPosX"
-            v-model="modelPos.x"
-            type="range"
-            name="modelPosX"
-            min="-5"
-            max="5"
-            step="0.1"
-          >
-        </div>
-        <div>
-          <label for="modelPosY">Y {{ modelPos.y }}</label>
-          <input
-            id="modelPosY"
-            v-model="modelPos.y"
-            type="range"
-            name="modelPosY"
-            min="-5"
-            max="5"
-            step="0.1"
-          >
-        </div>
-        <div>
-          <label for="modelPosZ">Z {{ modelPos.z }}</label>
-          <input
-            id="modelPosZ"
-            v-model="modelPos.z"
-            type="range"
-            name="modelPosZ"
-            min="-5"
-            max="5"
-            step="0.1"
-          >
-        </div>
-        <div>
-          <p>Camera:</p>
-          <p>X: {{ cameraPos.x }} | Y: {{ cameraPos.y }} | Z: {{ cameraPos.z }}</p>
-        </div>
-      </form>
-    </div>
   </div>
 </template>
 
---
+++ scripts/constants.ts
@@ -0,0 +1,30 @@
+export const POSITIONS = [
+  {
+    camera: { x: 0, y: 0.11, z: 0.41 },
+    model: { x: 0, y: -1, z: 0 }
+  },
+  {
+    camera: { x: 0.88, y: 0.22, z: 2.01 },
+    model: { x: 0, y: -0.45, z: 0 }
+  },
+  {
+    camera: { x: 0, y: 1.95, z: -0.02 },
+    model: { x: -1.15, y: -1, z: 0 }
+  },
+  {
+    camera: { x: -0.64, y: -0.42, z: -1.07 },
+    model: { x: 0, y: -0.5, z: 0 }
+  },
+  {
+    camera: { x: 0.40, y: 0.03, z: 0.72 },
+    model: { x: 0.15, y: -0.85, z: 0 }
+  },
+  {
+    camera: { x: -0.68, y: 1.82, z: 1.26 },
+    model: { x: -0.9, y: -0.05, z: 0 }
+  },
+  {
+    camera: { x: 0, y: 0, z: 3 },
+    model: { x: 0, y: -0.5, z: 0 }
+  }
+]
